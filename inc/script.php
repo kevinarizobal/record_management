d070b435b31e8ae6383c3b46d1c0d50c0ef0fabe
FDC.KEVIN <fdc.kevinar@gmail.com>
2024-12-09 00:44:42 +0800

    Update script.php

--- inc/script.php
+++ inc/script.php
@@ -1,3 +1,5 @@
+<script src="bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 <script>
 function setActive() {
     let navbar = document.getElementById('dashboard-menu');
